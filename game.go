2eaacab455a2917917eb781663c5016f5911f51f
Antoine Richard <arichard@restlet.com>
2016-07-25 23:27:45 +0200

    WIP:

--- game.go
+++ game.go
@@ -7,29 +7,27 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
+	"io/ioutil"
 	"log"
 	"math"
 	"strings"
 	"time"
-	"io/ioutil"
 
 	"github.com/golang/freetype"
 	"github.com/golang/freetype/truetype"
 	"golang.org/x/image/font"
-	mfont "golang.org/x/mobile/exp/font"
 	"golang.org/x/image/math/fixed"
+	"golang.org/x/mobile/asset"
 	"golang.org/x/mobile/event/size"
+	mfont "golang.org/x/mobile/exp/font"
 	"golang.org/x/mobile/exp/gl/glutil"
 	"golang.org/x/mobile/exp/sprite/clock"
 	"golang.org/x/mobile/geom"
 	"golang.org/x/mobile/gl"
-	"golang.org/x/mobile/asset"
 )
 
 const (
-	dpi           = 72
-	smallFontSize = 24
-	bigFontSize   = 96
+	dpi = 72
 )
 
 type Game struct {
@@ -95,64 +93,97 @@ func (g *Game) calcFrame() {
 
 }
 
-func (g *Game) Render(sz size.Event, glctx gl.Context, images *glutil.Images) {
-	
-	// TODO
-	height := 400
-	foreground := image.White
-	background := image.NewUniform(color.RGBA{0x35, 0x67, 0x99, 0xFF})
+type textSprite struct {
+	text            string
+	width           int
+	height          int
+	textColor       *image.Uniform
+	backgroundColor *image.Uniform
+	fontSize        float64
+	x               geom.Pt
+	y               geom.Pt
+	leftAligned     bool
+}
 
-	// Sprite to write text on
-	textSprite := images.NewImage(sz.WidthPx, height/*sz.HeightPx*/)
+func (ts textSprite) draw(sz size.Event, g *Game, dynamicText string) {
 
-	// Background to draw on
-	draw.Draw(textSprite.RGBA, textSprite.RGBA.Bounds(), background, image.ZP, draw.Src)
+	sprite := images.NewImage(ts.width, ts.height)
 
-	// Write the Loading... text on the sprite
-	loadingText := "Loading" + strings.Repeat(".", int(time.Now().Unix()%4))
+	// Background
+	draw.Draw(sprite.RGBA, sprite.RGBA.Bounds(), ts.backgroundColor, image.ZP, draw.Src)
+
 	d := &font.Drawer{
-		Dst: textSprite.RGBA,
-		Src: foreground,
+		Dst: sprite.RGBA,
+		Src: ts.textColor,
 		Face: truetype.NewFace(g.font, &truetype.Options{
-			Size:    bigFontSize,
+			Size:    ts.fontSize,
 			DPI:     dpi,
 			Hinting: font.HintingNone,
 		}),
 	}
-	dy := int(math.Ceil(bigFontSize * dpi / 72))
-	textWidth := d.MeasureString("Loading...")
-	d.Dot = fixed.Point26_6{
-		X: fixed.I(sz.Size().X/2) - (textWidth / 2),
-		Y: fixed.I(height/*sz.Size().Y*//2 + dy/2),
-	}
-	d.DrawString(loadingText)
-	
-	// Write the resolution on the sprite
-	resolutionText := fmt.Sprintf("%vpx * %vpx", sz.WidthPx, sz.HeightPx)
-	d = &font.Drawer{
-		Dst: textSprite.RGBA,
-		Src: foreground,
-		Face: truetype.NewFace(g.font, &truetype.Options{
-			Size:    smallFontSize,
-			DPI:     dpi,
-			Hinting: font.HintingNone,
-		}),
-	}
-	dy = int(math.Ceil(smallFontSize * dpi / 72))
-	d.Dot = fixed.Point26_6{
-		X: fixed.I(0),
-		Y: fixed.I(height - dy/2),
-	}
-	d.DrawString(resolutionText)
 
-	// Draw the text sprite on the screen
-	textSprite.Upload()
-	textSprite.Draw(
+	dy := int(math.Ceil(ts.fontSize * dpi / dpi))
+	textWidth := d.MeasureString(ts.text)
+
+	// TODO: API to improve...
+	if ts.leftAligned {
+		d.Dot = fixed.Point26_6{
+			X: fixed.I(0),
+			Y: fixed.I(ts.height/2 + dy/2),
+		}
+	} else { // centered
+		d.Dot = fixed.Point26_6{
+			X: fixed.I(sz.Size().X/2) - (textWidth / 2),
+			Y: fixed.I(ts.height/2 + dy/2),
+		}
+	}
+
+	// TODO: API to improve...
+	if dynamicText == "" {
+		d.DrawString(ts.text)
+	} else {
+		d.DrawString(dynamicText)
+	}
+
+	// Draw the sprite on the screen
+	sprite.Upload()
+	sprite.Draw(
 		sz,
-		geom.Point{},
-		geom.Point{X: sz.WidthPt},
-		geom.Point{Y: sz.HeightPt},
+		geom.Point{X: ts.x, Y: ts.y},
+		geom.Point{X: ts.x + sz.WidthPt, Y: ts.y},
+		geom.Point{X: ts.x, Y: ts.y + sz.HeightPt},
 		sz.Bounds())
-	textSprite.Release()
-	
+	sprite.Release()
+
+}
+
+func (g *Game) Render(sz size.Event, glctx gl.Context, images *glutil.Images) {
+
+	loading := &textSprite{
+		text:            "Loading...",
+		width:           sz.WidthPx,
+		height:          400,
+		textColor:       image.White,
+		backgroundColor: image.NewUniform(color.RGBA{0x35, 0x67, 0x99, 0xFF}),
+		fontSize:        96,
+		x:               0,
+		y:               0,
+	}
+
+	text := "Loading" + strings.Repeat(".", int(time.Now().Unix()%4))
+	loading.draw(sz, g, text)
+
+	resolution := &textSprite{
+		text:            fmt.Sprintf("%vpx * %vpx", sz.WidthPx, sz.HeightPx),
+		width:           sz.WidthPx,
+		height:          100,
+		textColor:       image.White,
+		backgroundColor: image.NewUniform(color.RGBA{0x31, 0xA6, 0xA2, 0xFF}),
+		fontSize:        24,
+		x:               0,
+		y:               140, // ???? Pt?
+		leftAligned:     true,
+	}
+	resolution.draw(sz, g, "")
+
 }
